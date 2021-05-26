998e01223ca40bc7a03cc0a43e9a82ede0cf3ab2
Gurleen Singh <grlsngh@gmail.com>
2021-05-26 14:12:35 -0400

    BR-06: Added back button to toolbar of Add update dish activity

--- app/src/main/java/com/example/bestrecipes/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/bestrecipes/view/activities/AddUpdateDishActivity.kt
@@ -3,10 +3,24 @@ package com.example.bestrecipes.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.bestrecipes.R
+import com.example.bestrecipes.databinding.ActivityAddUpdateDishBinding
 
 class AddUpdateDishActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
