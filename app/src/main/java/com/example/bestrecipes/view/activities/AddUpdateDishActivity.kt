768971033b3081901c71aa799a0eca6ac332d293
Gurleen <grlnsngh@gmail.com>
2021-08-04 13:03:52 -0400

    BR-09: Added customRationalDialog if user deny permission

--- app/src/main/java/com/example/bestrecipes/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/bestrecipes/view/activities/AddUpdateDishActivity.kt
@@ -8,6 +8,22 @@ import android.widget.Toast
 import com.example.bestrecipes.R
 import com.example.bestrecipes.databinding.ActivityAddUpdateDishBinding
 import com.example.bestrecipes.databinding.DialogCustomImageSelectionBinding
+import com.karumi.dexter.Dexter
+import android.Manifest
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.provider.MediaStore
+import android.provider.Settings
+import android.util.Log
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -46,13 +62,102 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
             DialogCustomImageSelectionBinding.inflate(layoutInflater)
         dialog.setContentView(binding.root)
         binding.tvCamera.setOnClickListener {
-            Toast.makeText(this, "Camera Clicked", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this).withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+//                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                //this is not required for newer devices
+                Manifest.permission.CAMERA
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    report?.let {
+                        //if report not empty, clean way
+                        if (report.areAllPermissionsGranted()) {
+                            Toast.makeText(
+                                this@AddUpdateDishActivity, "You have camera permission now",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+//                            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//                            startActivityForResult(intent, CAMERA)
+                        }
+                    }
+
+//                    else {
+//                        Log.e("<><><>","You have camera permission now - else")
+//                        showRationalDialogForPermissions()
+//                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+//                    token?.continuePermissionRequest()
+                    //to display default dialog again
+                }
+            }
+            ).onSameThread().check()
+
             dialog.dismiss()
         }
         binding.tvGallery.setOnClickListener {
-            Toast.makeText(this, "Gallery Clicked", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this).withPermission(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ).withListener(object : PermissionListener {
+                override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                    Toast.makeText(
+                        this@AddUpdateDishActivity, "You have gallery permission now",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                    Toast.makeText(
+                        this@AddUpdateDishActivity, "You have denied the gallery permission",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: PermissionRequest?,
+                    p1: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+
+            }).onSameThread().check()
+
             dialog.dismiss()
         }
         dialog.show()
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage(
+                "Permissions required for this feature." +
+                        "It can be enable under Application Settings"
+            )
+            .setPositiveButton("GO TO SETTINGS")
+            { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("CANCEL")
+            { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    companion object {
+        private const val CAMERA = 1
     }
 }
--- app/src/main/java/com/example/bestrecipes/view/activities/MainActivity.kt
+++ app/src/main/java/com/example/bestrecipes/view/activities/MainActivity.kt
@@ -1,10 +1,6 @@
 package com.example.bestrecipes.view.activities
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
